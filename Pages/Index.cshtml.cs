7d263069cf8b12901acc1e59286c5bc46ed15789
exyi <exyi@outlook.cz>
2020-09-17 11:26:11 +0000

    something

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Medallion.Shell;
 using Microsoft.AspNetCore.Mvc;
@@ -46,9 +47,10 @@ namespace ghostscript_runner.Pages
                 null => "",
                 "square" => rand.NextDouble() * 200 + "",
                 "lines" => rand.Next(5, 40) + "",
-                "star" => rand.Next(4, 15) + "",
-                "ruler" => rand.NextDouble() * 2 + 0.5 + "",
-                "pascal" => rand.Next(5, 20) + "",
+                "star" => rand.Next(6, 50) + "",
+                "ruler" => rand.NextDouble() * 2.5 + 0.5 + "",
+                "triangle" => rand.Next(4, 10) + "",
+                "chess" => rand.Next(5, 10) + "",
                 _ => throw new Exception("Neplatná úloha " + Task),
             };
 
@@ -58,6 +60,8 @@ namespace ghostscript_runner.Pages
 
             _logger.LogInformation("processing POST");
             string script = Request.Form["script"];
+            if (!ValidateScript(script))
+                return Page();
 
             var psFile = $"{wwwrootFolder}/out/{id}.ps";
             var pngFile = $"{wwwrootFolder}/out/{id}.png";
@@ -75,7 +79,7 @@ namespace ghostscript_runner.Pages
             _logger.LogInformation("Running gs {args}", string.Join(" ", args));
 
             var gsCmd = Command.Run("gs", args,
-                o => { o.Timeout(TimeSpan.FromSeconds(2005)); })
+                o => { o.Timeout(TimeSpan.FromSeconds(30)); })
                 .RedirectTo(outputLines)
                 .RedirectStandardErrorTo(outputLines)
                 ;
@@ -88,12 +92,24 @@ namespace ghostscript_runner.Pages
             return Page();
         }
 
+        public bool ValidateScript(string script)
+        {
+            if (Regex.IsMatch(script, @"\bshowpage\b"))
+            {
+                this.GsOutput = "Nepoužívejte tady prosím příkaz showpage. Smažte ho, stránka se vytiskne sama.";
+                return false;
+            }
+            return true;
+        }
+
         public async Task<IActionResult> OnPostPrint()
         {
             string script = Request.Form["script"];
+            if (!ValidateScript(script))
+                return Page();
             var psFile = $"{wwwrootFolder}/out/{Guid.NewGuid()}.ps";
             var input = GetTaskInput();
-            var script2 = input + "\n" + script + "\nshowpage\n";
+            var script2 = "%!PS\n" + input + "\n" + script + "\nshowpage\n";
             System.IO.File.WriteAllText(psFile, script2);
 
             System.IO.File.AppendAllLines("submit.log", new [] {
@@ -102,15 +118,21 @@ namespace ghostscript_runner.Pages
 
             var outputLines = new List<string>();
 
-
             var ncCmd = Command.Run("nc", new [] { "192.168.42.3", "9100" })
                 .RedirectFrom(new StringReader(script2))
                 .RedirectTo(outputLines)
                 .RedirectStandardErrorTo(outputLines)
                 ;
 
-            await System.Threading.Tasks.Task.Delay(1000);
+            await System.Threading.Tasks.Task.WhenAny(
+                ncCmd.Task,
+                System.Threading.Tasks.Task.Delay(1_000));
             ncCmd.Kill();
+
+            this.GsOutput = string.Join("\n", outputLines);
+
+            if (string.IsNullOrWhiteSpace(GsOutput))
+                this.GsOutput = "Vstup odeslán, tiskárna neoznámila žádnou chybu";
 
             return Page();
         }
